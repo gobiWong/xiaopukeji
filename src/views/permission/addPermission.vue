e4da23332d9cc4904ca54a24dd2cb99af7c32d44
gobiWong <543332860@qq.com>
2020-04-23 17:01:47 +0800

    完成权限管理页面

---
+++ src/views/permission/addPermission.vue
@@ -0,0 +1,112 @@
+<template>
+  <el-dialog
+    class="player-detail"
+    title="新增权限"
+    :visible.sync="addVisiable"
+    @close1="onClose"
+    :before-close="handleClose"
+    width="61.8%"
+  >
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="是否隐藏">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="是"></el-radio>
+          <el-radio label="否"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="权限名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="模块名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="控制器名">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="方法名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="权重大小">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="公共权限">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="是"></el-radio>
+          <el-radio label="否"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-button @click="onClose" size="small">取 消</el-button>
+      <el-button type="primary" @click="confirm" size="small">确定</el-button>
+    </el-form>
+  </el-dialog>
+</template>
+<script>
+export default {
+  name: "typeAdd",
+  props: {
+    addVisiable: {
+      default: false
+    },
+    type: {
+      default: 0 //0 行业类型 1 服务类型 2商品类型
+    }
+  },
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      },
+      formInline: {
+        user: "",
+        region: ""
+      }
+    };
+  },
+  methods: {
+    //点击叉叉按钮关闭，触发一'close1'方法名
+    handleClose() {
+      this.$emit("close");
+    },
+    //点击取消关闭子组件点取消按钮，触发一'close1'方法名
+    onClose() {
+      this.$emit("close");
+    },
+    confirm() {
+      //点击确定处理业务逻辑之后触发一个'success1'方法名
+      this.$emit("success");
+    }
+  }
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+.player-detail {
+  .box-card {
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    margin: 6.18px 0;
+    .show {
+      display: flex;
+      span {
+        width: 10%;
+      }
+    }
+    .other {
+      display: flex;
+      box-sizing: border-box;
+      .title {
+        width: 20%;
+      }
+    }
+  }
+}
+</style>
--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,5 +1,177 @@
 <template>
-  <div>
-    权限管理
+  <div class="app-container">
+    <!-- 搜素表头 -->
+    <div class="mm_button">
+      <el-form
+        :inline="true"
+        :model="formInline"
+        ref="serviceForm"
+        class="demo-form-inline"
+        size="small"
+      >
+        <el-form-item prop="region" label="权限名称">
+          <el-input v-model="formInline.region" placeholder="请输入内容"></el-input>
+        </el-form-item>
+        <el-form-item prop="region" label="所属分类">
+           <el-select v-model="formInline.data3" placeholder="全部">
+            <el-option label="全部" value="1"></el-option>
+            <el-option label="顶级分类" value="2"></el-option>
+            <el-option label="用户管理" value="3"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item prop="region" label="状态">
+          <el-select v-model="formInline.data3" placeholder="全部">
+            <el-option label="全部" value="1"></el-option>
+            <el-option label="显示" value="2"></el-option>
+            <el-option label="隐藏" value="3"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item prop="region" label="状态">
+          <el-select v-model="formInline.data3" placeholder="全部">
+            <el-option label="全部" value="1"></el-option>
+            <el-option label="启用" value="2"></el-option>
+            <el-option label="禁用" value="2"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            style="backgroundColor:#06A6F6;color:#fff;border-radius:8px;"
+            icon="el-icon-search"
+            @click="onSubmit('serviceForm')"
+            plain
+            size="small"
+          >查询</el-button>
+          <!-- <el-button
+            style="backgroundColor: #656FE7;color:#fff;border-radius:8px;"
+            icon="el-icon-delete"
+            @click="onReset('serviceForm')"
+            plain
+            size="small"
+          >重置</el-button>-->
+          <el-button
+            style="backgroundColor: #F67F77;color:#fff;border-radius:8px;"
+            icon="el-icon-circle-plus-outline"
+            size="small"
+            @click="addVisiable=true"
+          >新增权限</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <!-- 数据表格 -->
+    <div class="mm_table">
+      <el-table stripe :data="dataList" :header-cell-style="tableHead" :cell-style="tableCell">
+        <el-table-column prop="data1" label="权限名称"></el-table-column>
+        <el-table-column prop="data2" label="所属分类"></el-table-column>
+        <el-table-column prop="data1" label="模块名称"></el-table-column>
+        <el-table-column prop="data2" label="控制器名称"></el-table-column>
+        <el-table-column prop="data1" label="方法名称"></el-table-column>
+        <el-table-column prop="data2" label="是否显示"></el-table-column>
+        <el-table-column prop="data1" label="创建时间"></el-table-column>
+        <el-table-column prop="data2" label="更新时间"></el-table-column>
+        <el-table-column
+          prop="data3"
+          label="操作"
+          align="center"
+          width="180"
+          class-name="small-padding fixed-width"
+        >
+          <template slot-scope="scope">
+            <el-button type="primary" size="mini" @click="handleDetail(scope.row)">修改</el-button>
+            <el-button size="mini" type="danger" @click="handleDisabled(scope.row)">禁用</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <!-- 菜单弹框 -->
+    <edit-permission
+      :editVisiable="editVisiable"
+      @close="editVisiable=false"
+      @success="handleEditSuccess"
+    ></edit-permission>
+    <add-permission
+      :addVisiable="addVisiable"
+      @close="addVisiable=false"
+      @success="handleAddSuccess"
+    ></add-permission>
   </div>
 </template>
+<script>
+import editPermission from "./editPermission.vue";
+import addPermission from "./addPermission.vue";
+export default {
+  data() {
+    return {
+      addVisiable: false,
+      editVisiable: false,
+      dataList: [
+        {
+          data1: "陈1",
+          data2: "大哥1",
+          data3: "2019-05-07 15:22:12"
+        },
+        {
+          data1: "陈2",
+          data2: "大哥2",
+          data3: "2019-05-07 15:22:12"
+        },
+        {
+          data1: "陈3",
+          data2: "大哥3",
+          data3: "2019-05-07 15:22:12"
+        },
+        {
+          data1: "陈四",
+          data2: "大哥4",
+          data3: "2019-05-07 15:22:12",
+          data4: "4533"
+        }
+      ],
+      tableHead: {
+        "background-color": "#DCDFE6",
+        height: "30px",
+        "text-align": "center"
+      },
+      tableCell: {
+        "text-align": "center",
+        height: "20px"
+      },
+      formInline: {
+        region: ""
+      }
+    };
+  },
+  components: {
+    editPermission,
+    addPermission
+  },
+  created() {},
+
+  methods: {
+    handleDetail() {
+      this.editVisiable = true;
+    },
+    handleAddSuccess() {
+      this.addVisiable = false;
+      this.$message({
+        message: "新增权限成功",
+        type: "success"
+      });
+      // this.initList();
+    },
+    handleEditSuccess() {
+      this.editVisiable = false;
+      this.$message({
+        message: "修改权限成功",
+        type: "success"
+      });
+      // this.initList();
+    },
+    //禁用
+    handleDisabled() {
+      console.log("禁用");
+    }
+  }
+};
+</script>
+<style lang="scss" scope>
+</style>
