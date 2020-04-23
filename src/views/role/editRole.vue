aa59f0eac1bf95d3723728c8f1cdd6655b0457df
gobiWong <543332860@qq.com>
2020-04-23 17:51:40 +0800

    完成角色管理页面

---
+++ src/views/role/editRole.vue
@@ -0,0 +1,127 @@
+<template>
+  <el-dialog
+    class="player-detail"
+    title="菜单编辑"
+    :visible.sync="editVisiable"
+    @close1="onClose"
+    :before-close="handleClose"
+    width="61.8%"
+  >
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="标的管理">
+        <el-checkbox-group v-model="form.targetList">
+          <el-checkbox label="复选框 A"></el-checkbox>
+          <el-checkbox label="复选框 B"></el-checkbox>
+          <el-checkbox label="复选框 C"></el-checkbox>
+          <el-checkbox label="禁用"></el-checkbox>
+          <el-checkbox label="选中且禁用"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="主播管理">
+        <el-checkbox-group v-model="form.playerList">
+          <el-checkbox label="复选框 A"></el-checkbox>
+          <el-checkbox label="复选框 B"></el-checkbox>
+          <el-checkbox label="复选框 C"></el-checkbox>
+          <el-checkbox label="禁用"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="收益管理">
+        <el-checkbox-group v-model="form.earningsList">
+          <el-checkbox label="复选框 A"></el-checkbox>
+          <el-checkbox label="复选框 B"></el-checkbox>
+          <el-checkbox label="复选框 C"></el-checkbox>
+          <el-checkbox label="禁用"></el-checkbox>
+          <el-checkbox label="选中且禁用"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="系统设置">
+        <el-checkbox-group v-model="form.systemList">
+          <el-checkbox label="复选框 A"></el-checkbox>
+          <el-checkbox label="复选框 B"></el-checkbox>
+          <el-checkbox label="复选框 C"></el-checkbox>
+          <el-checkbox label="权限1"></el-checkbox>
+          <el-checkbox label="权限2"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="合作伙伴">
+        <el-checkbox-group v-model="form.partnersList">
+          <el-checkbox label="复选框 A"></el-checkbox>
+          <el-checkbox label="复选框 B"></el-checkbox>
+          <el-checkbox label="复选框 C"></el-checkbox>
+          <el-checkbox label="禁用"></el-checkbox>
+          <el-checkbox label="选中且禁用"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+
+      <el-button @click="onClose" size="small">取 消</el-button>
+      <el-button type="primary" @click="confirm" size="small">确定</el-button>
+    </el-form>
+  </el-dialog>
+</template>
+<script>
+export default {
+  name: "typeEdit",
+  props: {
+    editVisiable: {
+      default: false
+    },
+    type: {
+      default: 0 //0 行业类型 1 服务类型 2商品类型
+    }
+  },
+  data() {
+    return {
+      form: {
+        targetList: ["复选框 A"],
+        playerList: ["复选框 B"],
+        earningsList: ["复选框 C"],
+        systemList: ["复选框 A", "复选框 C"],
+        partnersList: ["禁用", "选中且禁用"]
+      },
+      formInline: {
+        user: "",
+        region: ""
+      }
+    };
+  },
+  methods: {
+    //点击叉叉按钮关闭，触发一'close1'方法名
+    handleClose() {
+      this.$emit("close");
+    },
+    //点击取消关闭子组件点取消按钮，触发一'close1'方法名
+    onClose() {
+      this.$emit("close");
+    },
+    confirm() {
+      //点击确定处理业务逻辑之后触发一个'success1'方法名
+      this.$emit("success");
+    }
+  }
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+.player-detail {
+  .box-card {
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    margin: 6.18px 0;
+    .show {
+      display: flex;
+      span {
+        width: 10%;
+      }
+    }
+    .other {
+      display: flex;
+      box-sizing: border-box;
+      .title {
+        width: 20%;
+      }
+    }
+  }
+}
+</style>
--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,5 +1,148 @@
 <template>
-  <div>
-    角色管理
+  <div class="app-container">
+    <!-- 搜素表头 -->
+    <div class="mm_button">
+      <el-form
+        :inline="true"
+        :model="formInline"
+        ref="serviceForm"
+        class="demo-form-inline"
+        size="small"
+      >
+        <!-- <el-form-item prop="region" label="菜单">
+          <el-input v-model="formInline.region" placeholder="请输入内容"></el-input>
+        </el-form-item>-->
+
+        <el-form-item>
+          <!-- <el-button
+            style="backgroundColor:#06A6F6;color:#fff;border-radius:8px;"
+            icon="el-icon-search"
+            @click="onSubmit('serviceForm')"
+            plain
+            size="small"
+          >查询</el-button>-->
+          <!-- <el-button
+            style="backgroundColor: #656FE7;color:#fff;border-radius:8px;"
+            icon="el-icon-delete"
+            @click="onReset('serviceForm')"
+            plain
+            size="small"
+          >重置</el-button>-->
+          <el-button
+            style="backgroundColor: #F67F77;color:#fff;border-radius:8px;"
+            icon="el-icon-circle-plus-outline"
+            size="small"
+            @click="addVisiable=true"
+          >新增角色</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <!-- 数据表格 -->
+    <div class="mm_table">
+      <el-table stripe :data="dataList" :header-cell-style="tableHead" :cell-style="tableCell">
+        <el-table-column prop="data1" label="名称"></el-table-column>
+        <el-table-column prop="data2" label="状态"></el-table-column>
+        <el-table-column prop="data3" label="创建时间"></el-table-column>
+        <el-table-column prop="data4" label="更新时间"></el-table-column>
+        <el-table-column
+          prop="data3"
+          label="操作"
+          align="center"
+          width="180"
+          class-name="small-padding fixed-width"
+        >
+          <template slot-scope="scope">
+            <el-button type="primary" size="mini" @click="handleDetail(scope.row)">修改</el-button>
+            <!-- <el-button size="mini" type="danger" @click="handleGover(scope.row)">禁用</el-button> -->
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <!-- 菜单弹框 -->
+    <edit-role
+      :editVisiable="editVisiable"
+      @close="editVisiable=false"
+      @success="handleEditSuccess"
+    ></edit-role>
+    <add-role :addVisiable="addVisiable" @close="addVisiable=false" @success="handleAddSuccess"></add-role>
   </div>
 </template>
+<script>
+import editRole from "./editRole.vue";
+import addRole from "./addRole.vue";
+export default {
+  data() {
+    return {
+      addVisiable: false,
+      editVisiable: false,
+      dataList: [
+        {
+          data1: "运营",
+          data2: "启用",
+          data3: "2019-05-07 15:22:12",
+          data4: "2019-05-08 15:22:12"
+        },
+        {
+          data1: "技术部",
+          data2: "启用",
+          data3: "2019-05-07 15:22:12",
+          data4: "2019-05-08 15:22:12"
+        },
+        {
+          data1: "商户",
+          data2: "启用",
+          data3: "2019-05-07 15:22:12",
+          data4: "2019-05-08 15:22:12"
+        },
+        {
+          data1: "超级管理员",
+          data2: "启用",
+          data3: "2019-05-07 15:22:12",
+          data4: "2019-05-08 15:22:12"
+        }
+      ],
+      tableHead: {
+        "background-color": "#DCDFE6",
+        height: "30px",
+        "text-align": "center"
+      },
+      tableCell: {
+        "text-align": "center",
+        height: "20px"
+      },
+      formInline: {
+        region: ""
+      }
+    };
+  },
+  components: {
+    addRole,
+    editRole
+  },
+  created() {},
+
+  methods: {
+    handleDetail() {
+      this.editVisiable = true;
+    },
+    handleAddSuccess() {
+      this.addVisiable = false;
+      this.$message({
+        message: "新增菜单成功",
+        type: "success"
+      });
+      // this.initList();
+    },
+    handleEditSuccess() {
+      this.editVisiable = false;
+      this.$message({
+        message: "修改菜单成功",
+        type: "success"
+      });
+      // this.initList();
+    }
+  }
+};
+</script>
+<style lang="scss" scope>
+</style>
